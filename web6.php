867860b719c7ccf7cdd7899200257ad51b0d0c27
H-ott <ndhoang65anm@gmail.com>
2025-08-30 17:52:18 +0700

    Create web6.php

---
+++ web6.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+<form method="GET" name="<?php echo basename($_SERVER['PHP_SELF']); ?>">
+<input type="TEXT" name="cmd" autofocus id="cmd" size="80">
+<input type="SUBMIT" value="Execute">
+</form>
+<pre>
+<?php
+echo ini_get("disable_functions");
+?>
+
+</pre>
+</body>
+</html>
